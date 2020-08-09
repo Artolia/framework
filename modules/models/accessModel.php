d5e60ca3bdfc0b24a34ea562b31d7e409a82ef39
Angel Artolia <lennet.valkyrie@gmail.com>
2020-08-09 11:26:31 +0700

    cek hak akses tidak muncul
    perbaikan button berdasarkan hak akses yang tidak muncul di karenakan query yang salah

--- modules/models/accessModel.php
+++ modules/models/accessModel.php
@@ -14,9 +14,10 @@ class accessModel{
 				INNER JOIN menus 
 					ON menus_id = menus.id 
 				WHERE 
-					menus_id = '".$group_id."'
+					groups_id = '".$group_id."'
 					AND page = '".$module."'
-					AND priv like '%".$accessRule."%'";				
+					AND priv like '%".$accessRule."%'";	
+				
         if($db->numRows($sql) > 0){
 			return true;
 		}else{
